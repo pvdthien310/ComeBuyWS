a7000c592daf592afbe2ee531091ee46523df33e
pvdthien310 <71278418+pvdthien310@users.noreply.github.com>
2022-12-10 18:53:26 +0700

    fix ws

--- WSServer.js
+++ WSServer.js
@@ -1,7 +1,7 @@
-const app = require('./index');
-const { Server} = require('socket.io');
-require('dotenv').config();
-const port = process.env.PORT || 8081;
+const app = require("./index");
+const { Server } = require("socket.io");
+require("dotenv").config();
+const port = 8080 || 8081;
 
 const httpServer = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
@@ -9,23 +9,21 @@ const httpServer = app.listen(port, () => {
 
 const ws = new Server(httpServer);
 
-app.post('/websocket', (req,res) => {
-    ws.emit(req.body.destination, JSON.stringify(req.body.data))
-    res.send("Successfully")
-})
+app.post("/websocket", (req, res) => {
+  ws.emit(req.body.destination, JSON.stringify(req.body.data));
+  res.send("Successfully");
+});
 
-process.on('uncaughtException', (err) => {
-  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+process.on("uncaughtException", (err) => {
+  console.log("UNCAUGHT EXCEPTION! 💥 Shutting down...");
   console.log(err.name, err.message, err);
   process.exit(1);
 });
 
-process.on('unhandledRejection', (err) => {
-  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+process.on("unhandledRejection", (err) => {
+  console.log("UNHANDLED REJECTION! 💥 Shutting down...");
   console.log(err);
   httpServer.close(() => {
     process.exit(1);
   });
-
 });
-
